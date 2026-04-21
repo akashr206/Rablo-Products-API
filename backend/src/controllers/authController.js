12760bdf7541e709b6abd35a27090226197f8c4a
akashr206 <akashr6514@gmail.com>
2026-04-21 13:19:27 +0530

    implement validations in the schema

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -67,7 +67,7 @@ exports.login = async (req, res) => {
         // if(!validateInput(email, password)){
         //     return res.status(400).json({ message: 'Invalid input' });
         // }
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select('+password');
         if (!user) {
             return res.status(401).json({ message: 'User not found' });
         }
